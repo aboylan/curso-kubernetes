bb83422a5ac739848dcd737b68f755d488bf1801
Alex Boylan <aboylan@users.noreply.github.com>
2024-11-18 13:25:25 -0600

    Implemnentando el controlador RestController y metodos handler GET

---
+++ msvc-usuarios/src/main/java/org/aboylan/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -0,0 +1,35 @@
+package org.aboylan.springcloud.msvc.usuarios.controllers;
+
+import org.aboylan.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.aboylan.springcloud.msvc.usuarios.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable Long id) {
+        Optional<Usuario> usuarioOptional = service.porId(id);
+        if (usuarioOptional.isPresent()) {
+            return ResponseEntity.ok(usuarioOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+}
