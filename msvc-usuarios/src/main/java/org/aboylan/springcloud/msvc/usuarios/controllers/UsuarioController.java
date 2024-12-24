5d343ac0cbfce99af31e8e20464e7c7bccf9125b
Alex Boylan <aboylan@users.noreply.github.com>
2024-12-23 21:11:16 -0600

    Optimizando Dockerfile

--- msvc-usuarios/src/main/java/org/aboylan/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/aboylan/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -38,7 +38,7 @@ public class UsuarioController {
         }
         if (!usuario.getEmail().isEmpty() && service.existePorEmail(usuario.getEmail())) {
             return ResponseEntity.badRequest().
-                    body(Collections.singletonMap("mensaje", "Ya existe un usuario con ese email electrónico!"));
+                    body(Collections.singletonMap("mensaje", "Ya existe!!! un usuario con ese email electrónico!"));
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
     }
