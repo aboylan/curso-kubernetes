1abb6e5e3d7fc5a2658789eefaf1d53976bfc8c2
Alex Boylan <aboylan@users.noreply.github.com>
2025-08-19 21:45:42 -0600

    Configurando el Servidor de Autorizacion

--- msvc-auth/src/main/java/org/aboylan/springcloud/msvc/auth/MsvcAuthApplication.java
+++ msvc-auth/src/main/java/org/aboylan/springcloud/msvc/auth/MsvcAuthApplication.java
@@ -1,4 +1,4 @@
-package org.aboylan.springcloud.msvc.msvc_auth;
+package org.aboylan.springcloud.msvc.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- msvc-auth/src/test/java/org/aboylan/springcloud/msvc/auth/MsvcAuthApplicationTests.java
+++ msvc-auth/src/test/java/org/aboylan/springcloud/msvc/auth/MsvcAuthApplicationTests.java
@@ -1,4 +1,4 @@
-package org.aboylan.springcloud.msvc.msvc_auth;
+package org.aboylan.springcloud.msvc.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
